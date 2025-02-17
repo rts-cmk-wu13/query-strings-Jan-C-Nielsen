207988ac739abff8a32b6aec0d5b37d203bade24
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-02-17 10:21:44 +0100

    Igen

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+
+function heartclick(id){
+  console.log("heartclick:" + id);
+}
+
 fetch("./data/destinations.json")
   .then(response => response.json())
   .then(y => {
-    console.log(y);
     const body = document.querySelector("body");
     const div = document.createElement("div");
     div.classList.add("grid");
-    let innerHTML = y.destinations.map(x => `
+    let innerHTML = y.destinations.map(x => `  
       <div class="card">  
       <h2>${x.title}</h2>  
      <div> <img src=img/${x.image}></div>
       <p>${x.destination}</p>
       
       <p>${x.facilities.join("<br>")}</p>  
-    <div class="card__more"><span>❤️</span><a href="detail.html?id=${x.id}">More</a></div>
+    <div class="card__more"><span  id="${x.id}" onclick="heartclick(${x.id})">❤️</span><a href="detail.html?id=${x.id}">More</a></div>
      </div>   `).join("");
+
     div.innerHTML = innerHTML;
-    console.log(innerHTML);
+   
     body.append(div);
+    //console.log(uniqueID);
+   // let heart = document.querySelector(`#${x.id}`);
+   // console.log(heart);
+    //heart.addEventListener("click", heartclick);
+    
   })
+
+
+  

5e046cdec3387b82e8be1685274f770f8137e9e1
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-02-18 10:23:23 +0100

    black hear

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+let Fav = []
+
+function Read() {
+  let temp = GetLocalStorage("heart");
+  console.log(temp)
+  if (temp == null)
+    return [];
+  else
+    return JSON.parse(temp);
+}
+
+
+Fav = Read();
+console.log(Fav);
+
 
 function SaveLocalStorage(key, value) {
+  console.log(value);
   localStorage.setItem(key, value);
 }
 
@@ -15,10 +31,21 @@ function DeleteLocalStorage(key) {
 
 
 function heartclick(id) {
-  console.log("heartclick:" + id);
-  SaveLocalStorage("heartclick:" + id, id);
+  let spade = document.querySelector(`#spade${id}`);
+  // console.log("heartclick:" + id);
+  if (Fav.includes(id)) {
+    Fav = Fav.filter(x => id != x);
+    console.log(Fav);
+   
+    spade.innerHTML = "🖤";
+    
+  }
+  else {
+    Fav.push(id);
+    document.querySelector(`#spade${id}`).innerHTML = "❤️";
+  }
+  SaveLocalStorage("heart", JSON.stringify(Fav));
 }
-
 
 
 fetch("./data/destinations.json")
@@ -30,12 +57,11 @@ fetch("./data/destinations.json")
     let innerHTML = y.destinations.map(x => `  
       <div class="card">  
       <h2>${x.title}</h2>  
-     <div> <img src=img/${x.image}></div>
+      <div> <img src=img/${x.image}></div>
       <p>${x.destination}</p>
-      
       <p>${x.facilities.join("<br>")}</p>  
-    <div class="card__more"><span  id="${x.id}" onclick="heartclick(${x.id})">❤️</span><a href="detail.html?id=${x.id}">More</a></div>
-     </div>   `).join("");
+      <div class="card__more"><span  id="spade${x.id}" onclick="heartclick(${x.id})">🖤</span><a href="detail.html?id=${x.id}">More</a></div>
+      </div>`).join("");
 
     div.innerHTML = innerHTML;
 
